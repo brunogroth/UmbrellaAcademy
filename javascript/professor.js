57b2b823bea1b59cdf38bb4198e78973d6ea12c8
Rodrigo453 <rodrigochicora.azevedo@gmail.com>
2022-05-05 17:26:08 -0300

    atuazando professor

--- javascript/professor.js
+++ javascript/professor.js
@@ -16,9 +16,46 @@ function relatorio(){
    document.getElementById('modalrelatorio').style.display= 'block';
 }
 
-function fecharModal(){
+function fecharrelatorio(){
    
    document.getElementById('modalrelatorio').style.display= 'none';
+}
+
+function conteudo(){
+   
+   document.getElementById('modalconteudo').style.display= 'block';
+}
+
+function fecharconteudo(){
+   
+   document.getElementById('modalconteudo').style.display= 'none';
+};
+
+function chamada(){
+   
+   document.getElementById('modalchamada').style.display= 'block';
+}
+
+function fecharchamada(){
+   
+   document.getElementById('modalchamada').style.display= 'none';
+};
+
+
+function alerta(){
+
+   if (id="rel")
+   {
+
+   alert("Arquivo enviado com sucesso!");
+   }
+
+   if (id="material")
+   {
+
+   alert("Arquivo enviado com sucesso!");
+   }
+
 }
 
 
