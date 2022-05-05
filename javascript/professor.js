9d1c3417b28bc3e8b5a29734040889babe506eae
Rodrigo453 <rodrigochicora.azevedo@gmail.com>
2022-05-05 09:59:49 -0300

    atualizando professor

--- javascript/professor.js
+++ javascript/professor.js
@@ -5,10 +5,20 @@ function interacao(){
    document.getElementById('idmodal').style.display= 'block';
 }
 
-function fecharModal(){
+function fecharagenda(){
    
    document.getElementById('idmodal').style.display= 'none';
 }
 
+
+function relatorio(){
+   
+   document.getElementById('modalrelatorio').style.display= 'block';
+}
+
+function fecharModal(){
+   
+   document.getElementById('modalrelatorio').style.display= 'none';
+}
 
 
