7182d9076e6186270e30092bb79a1647392723af
rodrigo453 <rodrigochicora.azevedo@gmail.com>
2022-06-24 10:10:19 -0300

    atualizando mensagens automaticas

--- chat/static/scripts/aluno/chat.js
+++ chat/static/scripts/aluno/chat.js
@@ -60,7 +60,7 @@ function getResponse() {
     let userText = $("#textInput").val();
 
     if (userText == "") {
-        userText = "I love Code Palace!";
+        userText = "Eu amo a prof Adriana";
     }
 
     let userHtml = '<p class="userText"><span>' + userText + '</span></p>';
@@ -94,7 +94,7 @@ function sendButton() {
 }
 
 function heartButton() {
-    buttonSendText("Heart clicked!")
+    buttonSendText("Agradecemos por mandar o coração <3!")
 }
 
 // Press enter to send a message
